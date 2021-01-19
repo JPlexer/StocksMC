24e899098a011300483586f8f4e230501aa17d60
JPlexer <jplexer.at.wcg@gmail.com>
2021-01-19 10:13:06 +0100

    Add files via upload

---
+++ src/main/java/omg/lol/jplexer/stocks/Stocks.java
@@ -0,0 +1,80 @@
+package omg.lol.jplexer.stocks;
+
+import net.milkbowl.vault.economy.Economy;
+import omg.lol.jplexer.stocks.commands.StocksCommand;
+import omg.lol.jplexer.stocks.events.TimeSchedulerEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Stocks extends JavaPlugin {
+    public Economy economy;
+    Connection connection;
+
+    Float currentSeed = null;
+
+    @Override
+    public void onDisable() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        economy = this.getServer().getServicesManager().getRegistration(Economy.class).getProvider();
+        prepareDatabase();
+        System.out.println(" ____  _____  ____  ____  _  __ ____ \n" +
+                            "/ ___\\/__ __\\/  _ \\/   _\\/ |/ // ___\\\n" +
+                            "|    \\  / \\  | / \\||  /  |   / |    \\\n" +
+                            "\\___ |  | |  | \\_/||  \\_ |   \\ \\___ |\n" +
+                            "\\____/  \\_/  \\____/\\____/\\_|\\_\\\\____/");
+        System.out.println("Stocks Loaded");
+        this.getServer().getScheduler().runTaskTimer(this, new TimeSchedulerEvent(this), 0, 200);
+        this.getCommand("stocks").setExecutor(new StocksCommand(this));
+    }
+
+    private void prepareDatabase() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:sqlite:stocks.db");
+            this.connection.createStatement().execute("PRAGMA foreign_keys=ON");
+            this.connection.createStatement().execute("CREATE TABLE IF NOT EXISTS lastDay(day INTEGER PRIMARY KEY)");
+            this.connection.createStatement().execute("CREATE TABLE IF NOT EXISTS stockPrices(id INTEGER PRIMARY KEY AUTOINCREMENT, seed FLOAT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getDatabase() {
+        return connection;
+    }
+    public static String getDollarValue(int cents) { return (cents < 0 ? "-" : "") + "$" + Math.abs(cents / 100) + "." + String.format("%02d", Math.abs(cents % 100)); }
+
+    public void setCurrentSeed(float seed) {
+        this.currentSeed = seed;
+    }
+
+    public Float currentSeed() {
+        return this.currentSeed;
+    }
+
+    public int getBuyPrice() {
+        return getBuyPrice(this.currentSeed);
+    }
+
+    public int getSellPrice() {
+        return getSellPrice(this.currentSeed);
+    }
+
+    public int getBuyPrice(float seed) {
+        return (int) (7400 * seed + 100 + (seed * 100));
+    }
+
+    public int getSellPrice(float seed) {
+        return (int) (7400 * seed + 100 - (seed * 100));
+    }
+}
---
+++ src/main/java/omg/lol/jplexer/stocks/events/TimeSchedulerEvent.java
@@ -0,0 +1,78 @@
+package omg.lol.jplexer.stocks.events;
+
+import omg.lol.jplexer.stocks.Stocks;
+import org.bukkit.World;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TimeSchedulerEvent implements Runnable {
+    Stocks plugin;
+    Connection connection;
+
+    long lastDay = -1;
+
+    public TimeSchedulerEvent(Stocks plugin) {
+        this.plugin = plugin;
+        connection = plugin.getDatabase();
+
+        try {
+            ResultSet results = connection.createStatement().executeQuery("SELECT day FROM lastDay");
+            if (results.next()) {
+                lastDay = results.getInt("day");
+            }
+            iterateStocks();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        World world = plugin.getServer().getWorld("world");
+        long currentDay = world.getFullTime() / 24000;
+        if (currentDay != lastDay) iterateStocks();
+    }
+
+    void iterateStocks() {
+        try {
+            World world = plugin.getServer().getWorld("world");
+            lastDay = world.getFullTime() / 24000;
+
+            //Add a new entry to the stock market
+            Float lastSeed = plugin.currentSeed();
+            if (lastSeed == null) {
+                //Only bother the database if we're starting up
+                ResultSet results = connection.createStatement().executeQuery("SELECT seed FROM stockPrices ORDER BY id DESC LIMIT 1");
+                if (results.next()) {
+                    lastSeed = results.getFloat("seed");
+                } else {
+                    lastSeed = 0.5f;
+                }
+            }
+
+            float newSeed = (float) ((Math.random() / 10) - 0.05 + lastSeed);
+            if (newSeed < 0) newSeed = 0;
+            if (newSeed > 1) newSeed = 1;
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO stockPrices(seed) VALUES(?)");
+            statement.setFloat(1, newSeed);
+            statement.executeUpdate();
+
+            plugin.setCurrentSeed(newSeed);
+            writeLastDay();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void writeLastDay() throws SQLException {
+        connection.createStatement().executeUpdate("DELETE FROM lastDay");
+
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO lastDay(day) VALUES(?)");
+        statement.setLong(1, lastDay);
+        statement.executeUpdate();
+    }
+}
